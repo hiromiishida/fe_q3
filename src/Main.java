8cffd9a99573f4ca4f2d54725a7e155f54c6cc83
hiromi <info@ishida-hiromi.com>
2022-11-01 20:01:17 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int value;
+
+        value = 100;
+        value = value % 3 + 1;
+
+        System.out.println(value);
+    }
+}
